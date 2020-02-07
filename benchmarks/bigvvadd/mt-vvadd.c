2bf9b956869b3c3b4bb5f2ef5b4e8665caa221e5
Troy Benjegerdes <troy.benjegerdes@sifive.com>
2020-02-07 23:25:34 +0000

    Add the bigvvadd (modified version of mt_vadd) for firesim debug

---
+++ benchmarks/bigvvadd/mt-vvadd.c
@@ -0,0 +1,81 @@
+// See LICENSE for license details.
+
+//**************************************************************************
+// Vector-vector add benchmark
+//--------------------------------------------------------------------------
+// Author  : Andrew Waterman
+// TA      : Christopher Celio
+// Student : 
+//
+// This benchmark adds two vectors and writes the results to a
+// third vector. The input data (and reference data) should be
+// generated using the vvadd_gendata.pl perl script and dumped
+// to a file named dataset.h 
+
+//--------------------------------------------------------------------------
+// Includes 
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+//--------------------------------------------------------------------------
+// Input/Reference Data
+
+typedef double data_t;
+
+#include "dataset.h"
+ 
+  
+//--------------------------------------------------------------------------
+// Basic Utilities and Multi-thread Support
+
+#include "util.h"
+   
+ 
+//--------------------------------------------------------------------------
+// vvadd function
+
+extern void __attribute__((noinline)) vvadd(int coreid, int ncores, size_t n, const data_t* x, const data_t* y, data_t* z);
+
+
+//--------------------------------------------------------------------------
+// Main
+//
+// all threads start executing thread_entry(). Use their "coreid" to
+// differentiate between threads (each thread is running on a separate core).
+  
+void thread_entry(int cid, int nc)
+{
+   // static allocates data in the binary, which is visible to both threads
+	static data_t result_data[DATA_SIZE];
+  
+	for (int count = 0; count < 20; count ++){ 
+		// First do out-of-place vvadd
+		barrier(nc);
+		stats(vvadd(cid, nc, DATA_SIZE, input1_data, input2_data, result_data); barrier(nc), DATA_SIZE);
+ 
+		if(cid == 0) {
+			int res = verifyDouble(DATA_SIZE, result_data, verify_data);
+			if(res) exit(res);
+		}
+
+		// Second do in-place vvadd
+		// Copying input
+		size_t i;
+		if(cid == 0) {
+			for (i = 0; i < DATA_SIZE; i++)
+				result_data[i] = input1_data[i];
+		}
+		barrier(nc);
+		stats(vvadd(cid, nc, DATA_SIZE, result_data, input2_data, result_data); barrier(nc), DATA_SIZE);
+ 
+   		if(cid == 0) {
+			int res = verifyDouble(DATA_SIZE, result_data, verify_data);
+			if(res) exit(res);
+		}
+	}  
+	barrier(nc);
+	exit(0);
+}
